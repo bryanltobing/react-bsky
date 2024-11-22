42c5e50a8987f4e4001f66f9a386d8e3c8e810a7
bryanltobing <bryantobing0@gmail.com>
2024-11-22 20:05:17 +0800

    feat: add example usage

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,102 +1,121 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "react-bsky/button";
-import styles from "./page.module.css";
+"use client";
+import { useEffect, useMemo, useState } from "react";
+import { BskyPost } from "react-bsky";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
+export function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
 
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+function extractBskyDetails(
+  url: string
+): { handle: string; id: string } | undefined {
+  const regex = /bsky\.app\/profile\/([\w.]+)\/post\/([\w]+)/;
+  const match = url.match(regex);
+  if (match && match[1] && match[2]) {
+    return {
+      handle: match[1],
+      id: match[2],
+    };
+  }
+}
+
+const DEFAULT_BSKY_POST_URI =
+  "https://bsky.app/profile/bryanprim.us/post/3la4tcw2npv2a";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>apps/docs/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [copied, setCopied] = useState(false);
+  const [postURI, setPostURI] = useState(DEFAULT_BSKY_POST_URI);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://turbo.build/repo/docs?utm_source"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const debouncedPostURI = useDebounce(postURI, 500);
+
+  const extractedBskyDetails = extractBskyDetails(
+    debouncedPostURI ?? DEFAULT_BSKY_POST_URI
+  );
+  const bskyPostProps = extractedBskyDetails ? extractedBskyDetails : undefined;
+
+  const code = useMemo(() => {
+    if (bskyPostProps) {
+      return `<BskyPost handle="${bskyPostProps.handle}" id="${bskyPostProps.id}" />`;
+    }
+    return "";
+  }, []);
+
+  const copyCode = () => {
+    if (code) {
+      navigator.clipboard.writeText(code);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "column", gap: "32px" }}>
+        <h1>Embed Bluesky post in your React application.</h1>
+
+        <div style={{ display: "flex", gap: "0.5rem" }}>
+          <input
+            type="text"
+            placeholder={DEFAULT_BSKY_POST_URI}
+            value={postURI}
+            onChange={(e) => setPostURI(e.target.value)}
+            style={{ padding: "16px", borderRadius: "8px", width: "100%" }}
+          />
+
+          {code && (
+            <button
+              style={{
+                flexShrink: "0",
+                paddingLeft: "2rem",
+                paddingRight: "2rem",
+                borderRadius: "8px",
+                color: "white",
+                backgroundColor: "#0A7AFF",
+                border: 0,
+                cursor: "pointer",
+              }}
+              onMouseEnter={(e) =>
+                (e.currentTarget.style.backgroundColor = "#0862CC")
+              }
+              onMouseLeave={(e) =>
+                (e.currentTarget.style.backgroundColor = "#0A7AFF")
+              }
+              onClick={copyCode}
+            >
+              {copied ? "Code Copied!" : "Copy Code"}
+            </button>
+          )}
         </div>
-        <Button appName="docs" className={styles.secondary}>
-          Open alert
-        </Button>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://turbo.build?utm_source=create-turbo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to turbo.build →
-        </a>
-      </footer>
+
+        <span style={{ textAlign: "center" }}>Result: </span>
+
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          {bskyPostProps ? (
+            <BskyPost {...bskyPostProps} />
+          ) : (
+            <p style={{ color: "red" }}>URL most likely invalid</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
